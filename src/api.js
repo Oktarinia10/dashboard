46d221ccae1e584c9c8bd2266a1f0ab89a4a5b5f
Oktarinia10 <rossa101410@gmail.com>
2023-10-01 23:18:21 +0700

    proses edit

--- src/api.js
+++ src/api.js
@@ -24,7 +24,7 @@ export async function fetchProducts(limit, offset) {
 export async function getProductsByID(id_product) {
     try {
         const url = base_url + `find-product` + `/` + id_product;
-        console.log(url);
+        // console.log(url);
         const headers = new Headers();
         headers.set('X-Api-Key', x_api_key);
 
@@ -34,12 +34,84 @@ export async function getProductsByID(id_product) {
         }
         const responseData = await response.json();
         
+        
         return responseData;
     } catch (error) {
         console.error('Terjadi kesalahan:', error);
         throw error; // Melempar kembali error untuk penanganan lebih lanjut
     }
 }
+ // proses edit data
+// export async function putEditProduct(id_product, formData) {
+//     try {
+//         const url = base_url_admin + `edit-product` + `/` + id_product;
+        
+//         const headers = new Headers();
+//         headers.set('X-Api-Key', x_api_key);
+//         headers.set('Authorization', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTY0NTU4MTMsImlhdCI6MTY5NjE2NzgxMywiaXNzIjoiMTZkYmNlMzYtYWUzNi00ZGE3LWE0YTQtZTcxYWI4OWU3ZDhmIiwic3ViIjoiUmVuZHJhIFRyaSBLdXN1bWEifQ.jPn8-osqgjiBALcrSpFM2iQKFUk0JOgrDbQbvNx2gMY');
+
+//         const body = new FormData();
+//         body.append('id_product', formData.id_product);
+//         body.append('name', formData.name);
+//         body.append('label', formData.label);
+//         body.append('description', formData.description);
+//         body.append('owner', formData.owner);
+//         body.append('price', formData.price);
+//         body.append('location', formData.location);
+//         body.append('thumbnail', formData.thumbnail);
+//         console.log(formData);
+
+//         const response = await fetch(url, {method: 'PUT', headers, body});
+//         if (!response.ok) {
+//             throw new Error('Gagal mengambil data dari API');
+//         }
+//         const responseData = await response.json();
+//         console.log(responseData);
+
+//         return responseData;
+//     } catch (error) {
+//         console.error('Terjadi kesalahan:', error);
+//         throw error; // Melempar kembali error untuk penanganan lebih lanjut
+//     }
+// }
+
+export async function putEditProduct(id_product, formData) {
+    try {
+        const url = `${base_url_admin}edit-product/${id_product}`;
+        
+        const headers = new Headers({
+            'X-Api-Key': x_api_key,
+            'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTY0NTU4MTMsImlhdCI6MTY5NjE2NzgxMywiaXNzIjoiMTZkYmNlMzYtYWUzNi00ZGE3LWE0YTQtZTcxYWI4OWU3ZDhmIiwic3ViIjoiUmVuZHJhIFRyaSBLdXN1bWEifQ.jPn8-osqgjiBALcrSpFM2iQKFUk0JOgrDbQbvNx2gMY',
+        });
+
+        const body = new FormData();
+        body.append('id_product', formData.id_product);
+        body.append('name', formData.name);
+        body.append('label', formData.label);
+        body.append('description', formData.description);
+        body.append('owner', formData.owner);
+        body.append('price', formData.price);
+        body.append('location', formData.location);
+        body.append('thumbnail', formData.thumbnail);
+
+        console.log(formData);
+
+        const response = await fetch(url, { method: 'PUT', headers, body });
+
+        if (!response.ok) {
+            throw new Error('Gagal mengambil data dari API');
+        }
+
+        const responseData = await response.json();
+        console.log(responseData);
+
+        return responseData;
+    } catch (error) {
+        console.error('Terjadi kesalahan:', error);
+        throw error; // Melempar kembali error untuk penanganan lebih lanjut
+    }
+}
+
 
 export async function addProducts(name_product, label_product, description, owner, price, location, file) {
     try {
--- src/pages/dataKatalog/edit.js
+++ src/pages/dataKatalog/edit.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import NavbarDashboard from "../../components/dashboard/navbar";
 import { getProductsByID } from '../../api';
+import { putEditProduct } from '../../api';
 
 
 function EditKatalog() {
   const {id_product} = useParams();
   const cdnLink = "https://terangnesia.sgp1.cdn.digitaloceanspaces.com/";
+  const [isAlertVisible, setAlertVisible] = useState(false);
   const [idProduct, setIdProducts] = useState([]);
   const [owner, setOwner] = useState([]);
   const [name, setName] = useState([]);
@@ -15,19 +17,42 @@ function EditKatalog() {
   const [price, setPrice] = useState([]);
   const [location, setLokasi] = useState([]);
   const [thumbnail, setThumbnail] = useState([]);
+  const [formData, setFormData] = useState({
+    id_product: id_product,
+    name: "",
+    label: "",
+    description: "",
+    owner: "",
+    price: "",
+    location: "",
+    // file: "", // Untuk mengunggah file gambar
+    thumbnail: "", // Untuk mengunggah file gambar
+  });
 
   useEffect(() => {
       const fetchDataFromAPI = async () => {
           try {
               const responseData = await getProductsByID(id_product);
-              setIdProducts(responseData.data);
-              setOwner(responseData.data.product.owner);
-              setName(responseData.data.product.name);
-              setLabel(responseData.data.product.label);
-              setDescription(responseData.data.product.description);
-              setPrice(responseData.data.product.price);
-              setLokasi(responseData.data.product.location);
-              setThumbnail(responseData.data.product.thumbnail);
+              
+              // setIdProducts(responseData.data);
+              // setOwner(responseData.data.product.owner);
+              // setName(responseData.data.product.name);
+              // setLabel(responseData.data.product.label);
+              // setDescription(responseData.data.product.description);
+              // setPrice(responseData.data.product.price);
+              // setLokasi(responseData.data.product.location);
+              // setThumbnail(responseData.data.product.thumbnail);
+              setFormData({ 
+                id_product: responseData.data.product.id_product,
+                name: responseData.data.product.name,
+                label: responseData.data.product.label,
+                description: responseData.data.product.description,
+                owner: responseData.data.product.owner,
+                price: responseData.data.product.price,
+                location: responseData.data.product.location,
+                // file: responseData.data.product.file,
+                thumbnail: responseData.data.product.thumbnail,
+                })
               
           } catch (error) {
               console.error('Terjadi kesalahan:', error);
@@ -36,7 +61,52 @@ function EditKatalog() {
 
       fetchDataFromAPI();
   }, [id_product]);
+
+  const handleInputChange = (e) => {
+    const { name, value, type, files } = e.target;
+    if (type === "file") {
+      setFormData({
+        ...formData,
+        [name]: files[0],
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
+  };
   
+  // proses update
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+
+    // Memanggil fungsi putEditProduct untuk mengirim data ke API
+    try {
+      const response = await putEditProduct(id_product, formData);
+      
+     
+      setAlertVisible(true);
+      // Respons dari API akan bisa digunakan di sini jika diperlukan
+      console.log("Respons dari API:", response);
+
+      // Setelah pengiriman data, Anda bisa mengosongkan form jika diperlukan
+      setFormData({
+        id_product: id_product,
+        name: "",
+        label: "",
+        description: "",
+        owner: "",
+        price: "",
+        location: "",
+        // file: "",
+        thumbnail: "",
+      });
+    } catch (error) {
+      console.error("Terjadi kesalahan:", error);
+    }
+  };
+
   return (
 <div>
     <NavbarDashboard/>
@@ -76,40 +146,64 @@ function EditKatalog() {
                     
                     <div className="row">
                       <div className="col-md-6">
-                        <form>
+                        <form onSubmit={handleUpdate} >
+                        <input type="text" hidden id="id_product" name="id_product" value={formData.id_product} onChange={handleInputChange}>
+                            </input>
                           <div className="form-group">
                             <label htmlFor="exampleInputEmail1">Nama Produk</label>
-                            <input type="text" className="form-control" value={name}></input>
+                            <input 
+                                type="text" 
+                                className="form-control"
+                                id="name"
+                                name="name" value={formData.name} 
+                                onChange={handleInputChange}>
+                            </input>
                           </div>
                           <div className="form-group"> 
                             <label htmlFor="exampleInputPassword1">Label</label>
-                            <input type="text" className="form-control" value={label}></input>
+                            <input type="text"  className="form-control"
+                                id="label"
+                                name="label" value={formData.label} onChange={handleInputChange}></input>
                           </div>
                           <div className="form-group"> 
                             <label htmlFor="exampleInputPassword1">Pemilik</label>
-                            <input type="text" className="form-control" value={owner}></input>
+                            <input type="text"  className="form-control"
+                                id="owner"
+                                name="owner" value={formData.owner} onChange={handleInputChange}></input>
                           </div>
                           <div className="form-group">
                             <label htmlFor="exampleFormControlTextarea1">Deskripsi</label>
-                            <textarea value={description} className="form-control" rows={3}></textarea>
+                            <textarea value={formData.description}  className="form-control"
+                                id="description"
+                                name="description" rows={3} onChange={handleInputChange}></textarea>
                           </div>
-                          <button type="submit" className="btn btn-primary">Submit</button>
-                        </form>
-                      </div>
-                      <div className="col-md-6">
-                        <form>
                           <div className="form-group">
                             <label>Harga</label>
-                            <input type="text" className="form-control" value={price}></input>
+                            <input type="text"
+                                className="form-control"
+                                id="price"
+                                name="price" value={formData.price} onChange={handleInputChange}></input>
                           </div>
                           <div className="form-group">
                             <label>Lokasi</label>
-                            <input type="text" className="form-control" value={location}></input>
+                            <input type="text"
+                                className="form-control"
+                                id="location"
+                                name="location" value={formData.location} onChange={handleInputChange}></input>
                           </div>
                           <div className="form-group">
                             <label>Gambar Thumbnail</label>
-                            <img src={cdnLink + thumbnail} height="50"></img>
+                            <input
+                                type="file"
+                                className="form-control"
+                                id="thumbnail"
+                                name="thumbnail"
+                                onChange={handleInputChange}
+                            />
+                            <img src={cdnLink + formData.thumbnail} 
+                                 height="50" onChange={handleInputChange}></img>
                           </div>
+                          <button onClick={handleUpdate} className="btn btn-primary">Submit</button>
                         </form>
                       </div>
                     </div>
